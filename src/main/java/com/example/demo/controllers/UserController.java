37ada1d01b6917798582db255ad64074057be65d
admin <admin@admins-imac.gateway>
2022-11-26 20:30:28 +1100

    2nd commit - Add Bcrypt encoder and jwt token

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -1,10 +1,8 @@
 package com.example.demo.controllers;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,6 +19,9 @@ import com.example.demo.model.requests.CreateUserRequest;
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
 	
 	@Autowired
 	private UserRepository userRepository;
@@ -41,13 +42,27 @@ public class UserController {
 	
 	@PostMapping("/create")
 	public ResponseEntity<User> createUser(@RequestBody CreateUserRequest createUserRequest) {
+		
+		boolean exist = userRepository.findByUsername(createUserRequest.getUsername()) != null;
+		if (exist) {
+			System.out.println("User already exists.");
+			return ResponseEntity.badRequest().build();
+		}
+		if (!createUserRequest.getPassword().equals(createUserRequest.getConfirmPassword())) {
+			System.out.println("Passwords do not match.");
+			return ResponseEntity.badRequest().build();
+		}
+		
 		User user = new User();
+
 		user.setUsername(createUserRequest.getUsername());
+		user.setPassword(passwordEncoder.encode(createUserRequest.getPassword()));
+
 		Cart cart = new Cart();
 		cartRepository.save(cart);
 		user.setCart(cart);
+
 		userRepository.save(user);
 		return ResponseEntity.ok(user);
 	}
-	
 }
